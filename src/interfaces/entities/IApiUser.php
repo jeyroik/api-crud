41e6070e57aaf111c5b627dc8acfe6a3a69de1ea
Jey Roik <jeyroik@gmail.com>
2026-01-09 15:52:22 +0300

    improve user storing

--- src/interfaces/entities/IApiUser.php
+++ src/interfaces/entities/IApiUser.php
@@ -5,5 +5,5 @@ interface IApiUser
 {
     public const FIELD__DOMAINS = '__domains__';
     public const FIELD__IS_ALLOWED = '__allowed__';
-    public const FIELD__ID = 'id';
+    public const FIELD__TOKEN = '__token__';
 }
