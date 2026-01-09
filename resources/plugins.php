1c4ec1f324ac75e6eb00661178bcd7d44f497d54
Jey Roik <jeyroik@gmail.com>
2026-01-09 16:54:02 +0300

    turn rewrite off

--- resources/plugins.php
+++ resources/plugins.php
@@ -4,6 +4,8 @@ use jeyroik\components\repositories\plugins\RepoPluginDates;
 use jeyroik\components\repositories\plugins\RepoPluginUuid;
 
 return [
-    RepoPluginUuid::class => [],
+    RepoPluginUuid::class => [
+        RepoPluginUuid::OPTION__REWRITE => RepoPluginUuid::REWRITE_OFF
+    ],
     RepoPluginDates::class => []
 ];
